32d0637271c834afa56e5fb0dc823f47932d2330
leena0203 <leenasarode02@gmail.com>
2020-12-30 02:02:15 +0530

    Refactored:removed unnecessary classes

--- src/main/java/com/capgemini/employeepayrollapp/repository/IDepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.employeepayrollapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capgemini.employeepayrollapp.model.Department;
-
-public interface IDepartmentRepository extends JpaRepository<Department, Long> {
-
-}
